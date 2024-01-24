e6f32e4cfb6e107a53cf9ec6602555874f661155
vasuguptaxperi <vasu.gupta@xperi.com>
2024-01-25 00:04:46 +0530

    updated variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,9 @@
 #varible are declared here
 variable "region" {
     type = string
-    default = "us-east-1"
 }
 
 variable "s3_bucket" {
     type = string
-    default = "mystaticwebsitehostingbucket"
 }
 
